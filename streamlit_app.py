2a834639d48ee42d0d88bd4b46b1215162618e14
Laxmi Narayana <lak.chennupati@gmail.com>
2026-05-24 00:47:49 -0400

    Update streamlit_app.py
    API for watermelon

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,6 @@ if ingredient_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/figs")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
