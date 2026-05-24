2519423ead4c4570d0f276f7635ab85ef50bd77b
Laxmi Narayana <lak.chennupati@gmail.com>
2026-05-24 00:26:08 -0400

    Update streamlit_app.py
    Fixed spaces infront of requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,6 @@ if ingredient_list:
         st.text(smoothiefroot_response)
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
- import requests
+import requests
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon])
 st.text(smoothiefroot_response)
