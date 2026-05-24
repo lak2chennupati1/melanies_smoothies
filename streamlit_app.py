f48008dd4634af766db370954fbb5741c96be6dd
Laxmi Narayana <lak.chennupati@gmail.com>
2026-05-24 00:41:33 -0400

    Update streamlit_app.py
    Changed fruit name to figs

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,5 +33,5 @@ if ingredient_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/figs")
 st.text(smoothiefroot_response.json())
